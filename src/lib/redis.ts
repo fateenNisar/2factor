d23fe6a36ab82df4ec202b2b6f42aa248e404014
fateenNisar <fateennisar121@gmail.com>
2025-03-22 23:56:39 +0530

    add GitHub Actions workflow for npm publish and enhance package.json scripts

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -3,29 +3,29 @@ import { Redis } from '@upstash/redis';
 import { z } from 'zod';
 import { rateLimitSchema } from '../utils/schema';
 import { TFError } from './error';
-export const rateLimit = async (data: z.infer<typeof rateLimitSchema>) => {
+export const rateLimit = async (data: z.infer<typeof rateLimitSchema>): Promise<void> => {
   try {
-  let { interval, limit, phoneNumber, url, token } = data;
-  const redis = new Redis({
-    url,
-    token,
-  });
-  const result = rateLimitSchema.safeParse(data);
-  if (!result.success) {
-    throw new Error(result.error.message);
-  }
-  const limiter = new Ratelimit({
-    redis: redis,
-    limiter: Ratelimit.slidingWindow(limit, interval as Duration),
-  });
-  const { success } = await limiter.limit(phoneNumber);
+    const { interval, limit, phoneNumber, url, token } = data;
+    const redis = new Redis({
+      url,
+      token,
+    });
+    const result = rateLimitSchema.safeParse(data);
+    if (!result.success) {
+      throw new Error(result.error.message);
+    }
+    const limiter = new Ratelimit({
+      redis: redis,
+      limiter: Ratelimit.slidingWindow(limit, interval as Duration),
+    });
+    const { success } = await limiter.limit(phoneNumber);
 
-  if (!success) {
-    throw new TFError('OTP limit reached');
-  }
+    if (!success) {
+      throw new TFError('OTP limit reached');
+    }
 
-  return;
-} catch (err) {
-  throw err;
-}
+    return;
+  } catch (err) {
+    throw err;
+  }
 };
